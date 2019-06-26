d247ddc582b7001c2105faf5c03318f5d519ba7b
Adin Schmahmann <adin.schmahmann@gmail.com>
2019-06-27 00:05:46 +0200

    wip trie

---
+++ itable.go
@@ -0,0 +1,36 @@
+package kbucket
+
+import (
+"github.com/libp2p/go-libp2p-core/peer"
+
+)
+
+// RoutingTable defines the routing table.
+type IRoutingTable interface {
+	Update(p peer.ID) (evicted peer.ID, err error)
+	Remove(p peer.ID)
+	Find(id peer.ID) peer.ID
+	NearestPeer(id ID) peer.ID
+	NearestPeers(id ID, count int) []peer.ID
+	Size() int
+	ListPeers() []peer.ID
+	Print()
+	SetPeerAdded(func(peer.ID))
+	SetPeerRemoved(func(peer.ID))
+}
+
+type RoutingTable struct {
+	tb IRoutingTable
+}
+
+func (rt *RoutingTable) Update(p peer.ID) (evicted peer.ID, err error) {return rt.tb.Update(p)}
+func (rt *RoutingTable) Remove(p peer.ID) {rt.tb.Remove(p)}
+func (rt *RoutingTable) Find(id peer.ID) peer.ID {return rt.tb.Find(id)}
+func (rt *RoutingTable) NearestPeer(id ID) peer.ID {return rt.tb.NearestPeer(id)}
+func (rt *RoutingTable) NearestPeers(id ID, count int) []peer.ID {return rt.tb.NearestPeers(id, count)}
+func (rt *RoutingTable) Size() int {return rt.tb.Size()}
+func (rt *RoutingTable) ListPeers() []peer.ID {return rt.tb.ListPeers()}
+func (rt *RoutingTable) Print() {rt.tb.Print()}
+func (rt *RoutingTable) SetPeerAdded(fn func(peer.ID)) {rt.tb.SetPeerAdded(fn)}
+func (rt *RoutingTable) SetPeerRemoved(fn func(peer.ID)) {rt.tb.SetPeerRemoved(fn)}
+
---
+++ largetrie.go
@@ -0,0 +1,129 @@
+package kbucket
+
+import (
+	"github.com/k-sone/critbitgo"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/peerstore"
+	"sync"
+	"time"
+)
+
+type TrieRoutingTable struct {
+	trie critbitgo.Trie
+
+	// ID of the local peer
+	local ID
+
+	// Blanket lock, refine later for better performance
+	tabLock sync.RWMutex
+
+	// latency metrics
+	metrics peerstore.Metrics
+
+	// Maximum acceptable latency for peers in this cluster
+	maxLatency time.Duration
+
+	// kBuckets define all the fingers to other nodes.
+	Buckets    []*Bucket
+	bucketsize int
+
+	// notification functions
+	PeerRemoved func(peer.ID)
+	PeerAdded   func(peer.ID)
+}
+
+func NewTrieRoutingTable(localID ID, latency time.Duration, m peerstore.Metrics) *TrieRoutingTable {
+	return &TrieRoutingTable{
+		local: localID,
+		maxLatency: latency,
+		metrics: m,
+		PeerAdded: func(id peer.ID) {},
+		PeerRemoved: func(id peer.ID) {},
+	}
+}
+
+func (rt *TrieRoutingTable) Update(p peer.ID) (evicted peer.ID, err error) {
+	if rt.metrics.LatencyEWMA(p) > rt.maxLatency {
+		// Connection doesnt meet requirements, skip!
+		return "", ErrPeerRejectedHighLatency
+	}
+
+	rt.trie.Insert(ConvertPeerID(p), p)
+	rt.PeerAdded(p)
+	return "", nil
+}
+
+func (rt *TrieRoutingTable) Remove(p peer.ID) {
+	_, ok := rt.trie.Delete(ConvertPeerID(p))
+	if ok {
+		rt.PeerRemoved(p)
+	}
+}
+
+func (rt *TrieRoutingTable) Find(id peer.ID) peer.ID {
+	pid := ConvertPeerID(id)
+	_, ok := rt.trie.Get(pid)
+	if ok {
+		return id
+	} else {
+		return ""
+	}
+}
+
+func (rt *TrieRoutingTable) NearestPeer(id ID) peer.ID {
+		foundID, val, _ := rt.trie.LongestPrefix(id)
+		if foundID == nil {
+			return ""
+		}
+		return val.(peer.ID)
+}
+
+func (rt *TrieRoutingTable) NearestPeers(id ID, count int) []peer.ID {
+	afterWalk := 0
+	beforeWalk := 0
+
+	peers := make([]peer.ID, 0, 40)
+
+	wid, _, _ := rt.trie.LongestPrefix(id)
+
+	rt.trie.Walk(wid, func(key []byte, value interface{}) bool {
+		peers = append(peers, value.(peer.ID))
+		afterWalk++
+		return afterWalk < count
+	})
+
+	rt.trie.RevWalk(wid, func(key []byte, value interface{}) bool {
+		if beforeWalk == 0{
+			beforeWalk ++
+			return true
+		}
+
+		peers = append(peers, value.(peer.ID))
+		beforeWalk++
+		return beforeWalk < count
+	})
+
+	return SortClosestPeers(peers, id)
+}
+
+func (rt *TrieRoutingTable) Size() int {
+	return rt.trie.Size()
+}
+
+func (rt *TrieRoutingTable) ListPeers() []peer.ID {
+	peers := make([]peer.ID, 0, 100)
+	k ,_, _:= rt.trie.LongestPrefix([]byte{0,0,0,0,0,0,0,0})
+	rt.trie.Walk(k, func(key []byte, value interface{}) bool {
+		peers = append(peers, value.(peer.ID))
+		return true
+	})
+	return peers
+}
+func (rt *TrieRoutingTable) Print() {}
+
+func (rt *TrieRoutingTable) SetPeerAdded(fn func(id peer.ID)) {
+	rt.PeerAdded = fn
+}
+func (rt *TrieRoutingTable) SetPeerRemoved(fn func(id peer.ID)) {
+	rt.PeerRemoved = fn
+}
